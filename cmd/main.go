266bfd3fa3472cfe3e3d7a9c8b147579323c219d
nikitin_sas <nikitinsasha323@gmail.com>
2025-11-19 19:03:31 +0300

    implement openapi spec to project initialize server logic

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"AvitoTest1/internal/handler"
+	"AvitoTest1/internal/server"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	handler := handler.NewHandler()
+	server := server.NewServer(handler)
+	//gracefull shutdown
+	serverError := make(chan error, 1)
+	go func() {
+		if err := server.Run(); err != nil {
+			serverError <- fmt.Errorf("server run failed: %w", err)
+			return
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	select {
+	case sig := <-quit:
+		log.Printf("Server shutting down with signal: %v", sig)
+	case err := <-serverError:
+		log.Printf("Server startup failed: %v", err)
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	log.Println("Server is shutting down...")
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Server shutdown error: %v", err)
+		return
+	}
+	log.Println("Server has shutted down successfully")
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module AvitoTest1
+
+go 1.23.4
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/oapi-codegen/runtime v1.1.2 // indirect
+)
